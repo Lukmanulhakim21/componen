51b0c30d8bc9483af90893a950a20fa227eb05cf
Lukmanulhakim21 <lh336544@gmail.com>
2021-06-18 17:31:53 +0700

    by abi

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <Title title="Catatan pengeluaran"/>
+  <FormMahasiswa @entri-mahasiswa = "entriMahasiswa($event)"/>
+  <!-- <FormMahasiswa @entri-mahasiswa = "entriMahasiswa($event)"/> -->
+  <ListMahasiswa v-if="dataMahasiswa.length>0" :dataMahasiswalist="dataMahasiswa"/>
+</template>
+
+<script>
+import Title from './components/Title.vue'
+import ListMahasiswa from './components/ListMahasiswa.vue'
+import FormMahasiswa from './components/FormMahasiswa.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Title,
+    ListMahasiswa,
+    FormMahasiswa
+  },
+  data(){
+    return{
+      dataMahasiswa : [
+        {nim : 'D111811111', nama : 'Mohammad Abi Haniah', kelas : 'TI B'},
+        {nim : 'D111811112', nama : 'Zainul Amri Harahap', kelas : 'TI B'},
+        {nim : 'D111811113', nama : 'Lukmanul hakim', kelas : 'TI B'}
+      ],
+    }
+  },
+// methods:{
+//   entriMahasiswa(event){
+//     this.dataMahasiswa.push(event);
+//     }
+//   }
+ methods:{
+    entriMahasiswa(event){
+      this.dataMahasiswa.push(event);
+    }
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
